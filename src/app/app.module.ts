1fef519b1519a800358ff42934f785f5af007cdd
AhamedBilal <bilalazmy0@gmail.com>
2021-04-07 09:11:17 +0530

    v0.3.4 image url cross fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NgxPhotoEditorModule} from '../../projects/ngx-photo-editor/src/lib/ngx-photo-editor.module';
+// import {NgxPhotoEditorModule} from '../../projects/ngx-photo-editor/src/lib/ngx-photo-editor.module';
 
 import {MatButtonModule, MatButtonToggleModule, MatIconModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
-// import {NgxPhotoEditorModule} from 'ngx-photo-editor';
+import {NgxPhotoEditorModule} from 'ngx-photo-editor';
 
 @NgModule({
   declarations: [

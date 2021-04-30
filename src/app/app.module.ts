1c45d43ac49c8061b6b42879cdbd4600865ef659
AhamedBilal <bilalazmy0@gmail.com>
2021-05-01 05:28:33 +0530

    v0.3.6 -peerdep fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {NgxPhotoEditorModule} from '../../projects/ngx-photo-editor/src/lib/ngx-photo-editor.module';
+// import {NgxPhotoEditorModule} from '../../projects/ngx-photo-editor/src/lib/ngx-photo-editor.module';
 import {MatButtonModule, MatButtonToggleModule, MatIconModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {NgxPhotoEditorModule} from 'ngx-photo-editor';
 
 @NgModule({
   declarations: [

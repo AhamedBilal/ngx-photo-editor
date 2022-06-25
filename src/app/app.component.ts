eb9dd84d84e9216191265fcf15e4ec2330d992f4
Ahamed Bilal <ahamedbilalazmy@gmail.com>
2022-06-25 09:57:06 +0530

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   constructor(private service: NgxPhotoEditorService) {}
 
   fileChangeHandler($event: any) {
-    this.service.open('https://images.pexels.com/photos/7913028/pexels-photo-7913028.jpeg#gfgfgf', {
+    this.service.open($event, {
       aspectRatio: 4 / 3,
       autoCropArea: 1
     }).subscribe(value => {
